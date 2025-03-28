5ceaba023065aa68df6fb27a336f6a06ed0ebcee
nsoterop <nsoterop@gmail.com>
2025-03-28 19:32:00 -0400

    Setup server

--- server/src/modules/app/app.service.ts
+++ server/src/modules/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'ASii Medical server is up and running!';
   }
 }
