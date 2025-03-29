473329d8e4348480370796042e888e074c3506e2
nsoterop <nsoterop@gmail.com>
2025-03-29 00:03:41 -0400

    Check format during server build

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -3,7 +3,7 @@ import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) {}
 
   @Get()
   getHello(): string {
