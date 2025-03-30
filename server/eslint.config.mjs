aecb33662bcd02191b82fb61e3988465ae80a53f
nsoterop <nsoterop@gmail.com>
2025-03-30 15:19:54 -0400

    Add content to public folder

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -21,6 +21,7 @@ export default tseslint.config(
       sourceType: 'module',
       parserOptions: {
         projectService: true,
+        // @ts-ignore
         tsconfigRootDir: import.meta.dirname,
       },
     },
