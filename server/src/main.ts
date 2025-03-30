42068a67eadccd8aa49d1a42b38e53f58aa44ed2
nsoterop <60040816+nsoterop@users.noreply.github.com>
2025-03-30 17:28:44 -0400

    Production Release (#1)
    * Setup server (#4)
    
    * Cleanup docker build for develop branch (#6)
    
    * Build cleanup
    
    * Run builds for pushes to feature branches
    
    * Run builds for pushes to feature branches
    
    * Rename pipeline files
    
    * Fix pipeline names
    
    * Feature/format check (#15)
    
    * Check format during server build
    
    * Check format during server build
    
    * Check format during server build
    
    * Add lint check to dockerfile
    
    * Setup basic client project (#18)
    
    * Feature/client cleanup (#20)
    
    * Setup basic client project
    
    * Add content to public folder
    
    * Feature/production deployment (#23)
    
    * Deployments per env
    
    * Rename build pipeline
    
    * Run build pipeline on dev runner
    
    * Run build pipeline on dev runner
    
    * Run build pipeline on dev runner
    
    * Run builds on ubuntu
    
    * Feature/production deployment (#24)
    
    * Deployments per env
    
    * Run build pipeline on dev runner
    
    * Run build pipeline on dev runner
    
    * Run build pipeline on dev runner
    
    * Run builds on ubuntu
    
    * Run builds on ubuntu

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((e) => console.log(e));
