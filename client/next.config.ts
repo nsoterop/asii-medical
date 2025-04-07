ff785442e05012dcd3dee77f3e2effafe75a4814
nsoterop <nsoterop@gmail.com>
2025-04-07 14:27:42 -0400

    Add turbo config

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    turbo: {
+      // logLevel: 'info', // 'error', 'warn', 'info', 'debug'
+    },
+  },
 };
 
 export default nextConfig;
