c51ef2406178f08202e150cd5bb57110cd5d34b4
nsoterop <nsoterop@gmail.com>
2025-04-07 22:53:21 -0400

    Test next rewrite

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,12 +4,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   async rewrites() {
-    return [
+    const rewrites = [
       {
         source: '/api/:path*',
         destination: `http://ec2-3-95-177-173.compute-1.amazonaws.com:3001/api/:path*`,
       },
     ]
+    console.log('Configured rewrites:', rewrites); // Visible during build
+    return rewrites;
   },
 };
 
