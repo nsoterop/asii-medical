1cf7751e95b92611d59c87f4bb1d7945e384bf44
nsoterop <nsoterop@gmail.com>
2025-04-07 22:40:53 -0400

    Test next rewrite

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const PUBLIC_URL = "http://localhost:3000";
+
 const nextConfig: NextConfig = {
   output: "standalone",
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL}/api/:path*`,
+        destination: `${process.env.API_BASE_URL || PUBLIC_URL}/api/:path*`,
       },
     ]
   },
